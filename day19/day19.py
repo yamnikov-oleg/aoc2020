7fc0329794efa8a80210bbeb1ccceec5db8a0f74
Oleg Yamnikov <oleg@oyam.dev>
2020-12-20 19:35:17 +0500

    Solve day 19 part 2

--- day19/day19.py
+++ day19/day19.py
@@ -1,40 +1,120 @@
 import re
 from dataclasses import dataclass
-from typing import Dict, Iterator, List
+from typing import Dict, List
 
 
 class Rule:
-    def iter_valid_values(self) -> Iterator[str]:
-        raise NotImplementedError("iter_valid_values")
+    """
+    The base class for other string validation rules.
+    Provides single interface for them.
+    """
+    def consume(self, line: str) -> List[str]:
+        """
+        Parses substrings, which are valid by this rule, from the start of the line.
+
+        :returns: the list of all possible remainders left after parsing all
+        possible substrings.
+
+        Some examples:
+
+        * If the line is invalid, returns an empty list (there is no valid substring)::
+
+            rule.consume("aba")  # => []
+            # "aba" is invalid
+
+        * If there is only one matching substring, returns a list of the single remainder::
+
+            rule.consume("aba")  # => ["a"]
+            # "ab" matches the rule, "a" is the remainder
+
+        * If there are multiple matches, returns multiple remainders::
+
+            rule.consume("aba")  # => ["a", "ba"]
+            # "a" and "ab" both match the rule
+
+        * If the input line matches the rule entirely, the output list will contain
+          an empty string, which means that the line was consumes entirely::
+
+            rule.consume("aba")  # => ["a", ""]
+            # "ab" and "aba" both match
+        """
+        raise NotImplementedError("consume")
 
 
 @dataclass
 class CharRule(Rule):
+    """
+    A rule which matches a single character.
+    """
     char: str
 
-    def iter_valid_values(self) -> Iterator[str]:
-        yield self.char
+    def consume(self, line: str) -> List[str]:
+        if not line:
+            return []
+
+        if line[0] != self.char:
+            return []
+
+        return [line[1:]]
 
 
 @dataclass
 class ComplexRule(Rule):
+    """
+    A rule which matches one of the sequences of rules (the subrules).
+    """
+    # Each item in the list is a subrule.
+    # A subrule is a sequence of rules that matches a single string.
     subrules: List[List[Rule]]
 
-    def iter_valid_values(self) -> Iterator[str]:
-        def concat_rules(rules):
-            if len(rules) == 0:
-                yield ""
-                return
-
-            for subvalue in concat_rules(rules[1:]):
-                for value in rules[0].iter_valid_values():
-                    yield value + subvalue
-
+    def consume(self, line: str) -> List[str]:
+        remainders = []
         for subrule in self.subrules:
-            yield from concat_rules(subrule)
+            remainders.extend(consume_seq(subrule, line))
+
+        return remainders
 
 
-def parse_rules(content: str) -> Dict[int, Rule]:
+def consume_seq(rules: List[Rule], line: str) -> List[str]:
+    """
+    Consumes the line by the each rule in the sequence one by one.
+    Returns all remainders after consuming all valid substrings.
+
+    Example:
+
+    Given that rule1 matches "a" and rule2 matches "b" or "bb", calling::
+
+        consume_seq([rule1, rule2], "abba")
+
+    would return (possible in different order)::
+
+        ["a", "ba"]
+
+    because "abb" and "ab" match the sequence of rule1 and rule2.
+    """
+    if not rules:
+        return []
+
+    if len(rules) == 1:
+        return rules[0].consume(line)
+
+    seq_remainders = []
+    for remainder in rules[0].consume(line):
+        seq_remainders.extend(consume_seq(rules[1:], remainder))
+
+    return seq_remainders
+
+
+def split_rule_texts(content: str) -> Dict[int, str]:
+    """
+    Split the rules file into a dictionary, which matches the number of each rule
+    to the text of said rule.
+
+    Example::
+
+        split_rule_texts('0: "a"\n1: 0 0')
+        # => {0: '"a"', 1: '0 0'}
+    """
     generic_rule_re = re.compile(r'^(\d+): (.+)$')
     rule_texts: Dict[int, str] = {}
     for line in content.splitlines():
@@ -46,52 +126,109 @@ def parse_rules(content: str) -> Dict[int, Rule]:
         rule_text = match.group(2)
         rule_texts[rule_number] = rule_text
 
-    parsed_rules: Dict[int, Rule] = {}
+    return rule_texts
+
+
+def parse_complex_rule_text(text: str) -> List[List[int]]:
+    """
+    Parses the text of a complex rule into a list of lists of number.
+
+    Example::
+
+        parse_complex_rule_text('1 2 | 3 4')
+        # => [[1, 2], [3, 4]]
+    """
+    subrule_numbers = []
+    subrule_texts = text.split('|')
+    for subrule_text in subrule_texts:
+        subrule = []
+        for ref_number_str in subrule_text.strip().split(' '):
+            ref_number = int(ref_number_str)
+            subrule.append(ref_number)
+        subrule_numbers.append(subrule)
+
+    return subrule_numbers
+
+
+def parse_rule_texts(rule_texts: Dict[int, str]) -> Dict[int, Rule]:
+    """
+    Parses rule texts.
+
+    :param rule_texts: a dictionary of rule texts, as returned by split_rule_texts.
+    :returns: a dictionary which maps a rule's number to the rule.
+    """
+    rules: Dict[int, Rule] = {}
+
     char_rule_re = re.compile(r'^"(.+)"$')
     complex_rule_re = re.compile(r'^[\d\s\|]+$')
 
     def parse_rule_text(number: int) -> Rule:
-        if number in parsed_rules:
-            return parsed_rules[number]
+        if number in rules:
+            return rules[number]
 
         if match := char_rule_re.match(rule_texts[number]):
             rule = CharRule(match.group(1))
         elif match := complex_rule_re.match(rule_texts[number]):
-            subrule_texts = rule_texts[number].split('|')
-            subrules = []
-            for subrule_text in subrule_texts:
+            rule = ComplexRule([])
+            subrule_numbers = parse_complex_rule_text(rule_texts[number])
+            for subrule_numbers_item in subrule_numbers:
                 subrule = []
-                for ref_number_str in subrule_text.strip().split(' '):
-                    ref_number = int(ref_number_str)
-                    subrule.append(parse_rule_text(ref_number))
-                subrules.append(subrule)
-            rule = ComplexRule(subrules)
+                for ref_number in subrule_numbers_item:
+                    if ref_number == number:
+                        subrule.append(rule)
+                    else:
+                        subrule.append(parse_rule_text(ref_number))
+                rule.subrules.append(subrule)
         else:
             raise ValueError(f"Invalud rule: {rule_texts[number]}")
 
-        parsed_rules[number] = rule
+        rules[number] = rule
         return rule
 
     parse_rule_text(0)
 
     for rule_number in rule_texts.keys():
-        if rule_number not in parsed_rules:
+        if rule_number not in rules:
             raise ValueError(f"Rule {rule_number} is not referenced from rule 0")
 
-    return parsed_rules
+    return rules
+
+
+def is_valid(rule: Rule, line: str) -> bool:
+    """
+    Validates the line against the rule.
+
+    :returns: True if the line matches the rule.
+    """
+    remainders = rule.consume(line)
+    return "" in remainders
 
 
 def main():
     with open('./input.txt') as f:
         content = f.read()
         rules_content, values_content = content.split('\n\n')
-        rules = parse_rules(rules_content)
-        values = values_content.splitlines()
+
+    rule_texts = split_rule_texts(rules_content)
+    rules = parse_rule_texts(rule_texts)
+    values = values_content.splitlines()
 
     valid_values = []
-    for valid_value in rules[0].iter_valid_values():
-        if valid_value in values:
-            valid_values.append(valid_value)
+    for value in values:
+        if is_valid(rules[0], value):
+            valid_values.append(value)
+
+    print(f"Valid values: {len(valid_values)}")
+
+    rec_rule_texts = rule_texts.copy()
+    rec_rule_texts[8] = '42 | 42 8'
+    rec_rule_texts[11] = '42 31 | 42 11 31'
+    rec_rules = parse_rule_texts(rec_rule_texts)
+
+    valid_values = []
+    for value in values:
+        if is_valid(rec_rules[0], value):
+            valid_values.append(value)
 
     print(f"Valid values: {len(valid_values)}")
 
--- day19/test_day19.py
+++ day19/test_day19.py
@@ -1,6 +1,45 @@
 import pytest
 
-from day19 import CharRule, ComplexRule, parse_rules
+from day19 import CharRule, ComplexRule, is_valid, parse_rules
+
+# 0: 1 2
+# 1: "a"
+# 2: 1 3 | 3 1
+# 3: "b"
+TEST_RULE_1 = ComplexRule([
+    [CharRule("a"), ComplexRule([
+        [CharRule("a"), CharRule("b")], [CharRule("b"), CharRule("a")],
+    ])],
+])
+
+# 0: 4 1 5
+# 1: 2 3 | 3 2
+# 2: 4 4 | 5 5
+# 3: 4 5 | 5 4
+# 4: "a"
+# 5: "b"
+TEST_RULE_2 = ComplexRule([
+    [CharRule('a'), ComplexRule([
+        [
+            ComplexRule([
+                [CharRule('a'), CharRule('a')],
+                [CharRule('b'), CharRule('b')],
+            ]), ComplexRule([
+                [CharRule('a'), CharRule('b')],
+                [CharRule('b'), CharRule('a')],
+            ])
+        ], [
+            ComplexRule([
+                [CharRule('a'), CharRule('b')],
+                [CharRule('b'), CharRule('a')],
+            ]),
+            ComplexRule([
+                [CharRule('a'), CharRule('a')],
+                [CharRule('b'), CharRule('b')],
+            ])
+        ]
+    ]), CharRule('b')],
+])
 
 
 @pytest.mark.parametrize("content,rules", [
@@ -28,39 +67,18 @@ def test_parse_rules(content, rules):
 
 
 @pytest.mark.parametrize("rule,value_set", [
-    (
-        ComplexRule([
-            [CharRule("a"), ComplexRule([
-                [CharRule("a"), CharRule("b")], [CharRule("b"), CharRule("a")],
-            ])],
-        ]),
-        {"aab", "aba"},
-    ),
-    (
-        ComplexRule([
-            [CharRule('a'), ComplexRule([
-                [
-                    ComplexRule([
-                        [CharRule('a'), CharRule('a')],
-                        [CharRule('b'), CharRule('b')],
-                    ]), ComplexRule([
-                        [CharRule('a'), CharRule('b')],
-                        [CharRule('b'), CharRule('a')],
-                    ])
-                ], [
-                    ComplexRule([
-                        [CharRule('a'), CharRule('b')],
-                        [CharRule('b'), CharRule('a')],
-                    ]),
-                    ComplexRule([
-                        [CharRule('a'), CharRule('a')],
-                        [CharRule('b'), CharRule('b')],
-                    ])
-                ]
-            ]), CharRule('b')],
-        ]),
-        {"aaaabb", "aaabab", "abbabb", "abbbab", "aabaab", "aabbbb", "abaaab", "ababbb"},
-    ),
+    (TEST_RULE_1, {"aab", "aba"}),
+    (TEST_RULE_2, {"aaaabb", "aaabab", "abbabb", "abbbab", "aabaab", "aabbbb", "abaaab", "ababbb"}),
 ])
 def test_rule_iter_valid_values(rule, value_set):
     assert set(rule.iter_valid_values()) == value_set
+
+
+@pytest.mark.parametrize("rule,value,result", [
+    (TEST_RULE_1, "aab", True),
+    (TEST_RULE_1, "abb", False),
+    (TEST_RULE_1, "aaba", False),
+    (TEST_RULE_1, "aa", False),
+])
+def test_is_valid(rule, value, result):
+    assert is_valid(rule, value) == result
